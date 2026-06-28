4cde637d5d448f14c0edf8de470ef7511fc5b3eb
EugeneHard-cmd <eugenehard69@gmail.com>
2026-06-28 17:52:06 +0200

    Made changes to admin.py file and registered all the models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Category, Book
 # Register your models here.
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', "slug")
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [BookInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "price", "stock")
+    list_filter = ("category", "author")
+    search_fields = ("title", "author", "description")
+    list_editable = ("price", "stock")
