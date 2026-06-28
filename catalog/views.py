f848b205223686b6da8e09102c26fcd624b09e67
EugeneHard-cmd <eugenehard69@gmail.com>
2026-06-28 20:16:42 +0200

    Added template, view to catalog dashboard and connected url

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.db.models import Count, Q
+from .models import Book, Category
 # Create your views here.
+
+def catalog_dashboard(request):
+    available_books = Book.objects.filter(stock__gt=0)
+    search_results = Book.objects.filter(Q(title__icontains="Django") | Q(author__icontains="Python"))
+    categories_with_count = Category.objects.annotate(books_count=Count('books'))
+    context = {
+        "available_books": available_books,
+        "search_results": search_results,
+        "categories": categories_with_count,
+    }
+
+    return render(request, 'catalog/dashboard.html', context)
